d97691feef1bb98b8ee9cd2dfc0a094de5d61288
Jim Connell <james.d.connell@gmail.com>
2021-01-13 06:21:42 -0500

    initial frontend tester for VM.

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+import { JackVirtualMachine } from "jackvm-wasm";
+
+const vm = JackVirtualMachine.new();
+vm.load("push constant 3\npush constant 4\nadd");
+vm.tick();
+vm.tick();
+vm.tick();
+console.log("**** value from VM: " + vm.peek(256));
