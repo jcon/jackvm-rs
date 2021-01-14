b046b69411bac5459bcf1eecdc17bc521fd69d13
Jim Connell <james.d.connell@gmail.com>
2021-01-14 18:46:50 -0500

    add a flow control test with fibonacci series.

--- tests/integration.rs
+++ tests/integration.rs
@@ -242,4 +242,71 @@ mod test {
         assert_eq!(jack_vm.peek(0), 257);
         assert_eq!(jack_vm.peek(256), 6);
     }
+
+    #[test]
+    pub fn test_program_flow_fibonacci_series() {
+        let mut jack_vm = compile_program("
+            // Puts the first argument[0] elements of the Fibonacci series
+            // in the memory, starting in the address given in argument[1].
+            // Argument[0] and argument[1] are initialized by the test script
+            // before this code starts running.
+
+            push argument 1
+            pop pointer 1           // that = argument[1]
+
+            push constant 0
+            pop that 0              // first element in the series = 0
+            push constant 1
+            pop that 1              // second element in the series = 1
+
+            push argument 0
+            push constant 2
+            sub
+            pop argument 0          // num_of_elements -= 2 (first 2 elements are set)
+
+            label MAIN_LOOP_START
+
+            push argument 0
+            if-goto COMPUTE_ELEMENT // if num_of_elements > 0, goto COMPUTE_ELEMENT
+            goto END_PROGRAM        // otherwise, goto END_PROGRAM
+
+            label COMPUTE_ELEMENT
+
+            push that 0
+            push that 1
+            add
+            pop that 2              // that[2] = that[0] + that[1]
+
+            push pointer 1
+            push constant 1
+            add
+            pop pointer 1          // that += 1
+
+            push argument 0
+            push constant 1
+            sub
+            pop argument 0         // num_of_elements--
+
+            goto MAIN_LOOP_START
+
+            label END_PROGRAM
+        ");
+
+        jack_vm.poke(0, 256);
+        jack_vm.poke(1, 300);
+        jack_vm.poke(2, 400);
+        jack_vm.poke(400, 6);
+        jack_vm.poke(401, 3000);
+
+        for _ in 0..1100 {
+            jack_vm.tick();
+        }
+
+        assert_eq!(jack_vm.peek(3000), 0);
+        assert_eq!(jack_vm.peek(3001), 1);
+        assert_eq!(jack_vm.peek(3002), 1);
+        assert_eq!(jack_vm.peek(3003), 2);
+        assert_eq!(jack_vm.peek(3004), 3);
+        assert_eq!(jack_vm.peek(3005), 5);
+    }
 }
