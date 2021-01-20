b7b6e64bf175413b932fbe72970f42d46f7674e4
Jim Connell <james.d.connell@gmail.com>
2021-01-20 13:23:35 -0500

    add some screen debug capabilities.

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,7 @@ const vm = JackVirtualMachine.new(screenBuffer);
 
 const mainCanvas = document.getElementById('screen');
 const mainContext = mainCanvas.getContext('2d');
-const imageData = new ImageData(256, 512);
+const imageData = new ImageData(512, 256);
 imageData.data.set(screenBytes);
 
 let progEl = document.querySelector("#editor");
@@ -15,10 +15,31 @@ console.log(progEl.value.split('\n').map(s => s.trim()));
 let runEl = document.querySelector("#run");
 
 let memoryCells = {};
-let memoryCellIds = [0, 1, 2, 256, 257, 258, 259, 260, 261, 262, 16384, 16416, 16448, 24575]
+let memoryCellIds = [0, 1, 2, 256, 257, 258, 259, 260, 261, 262]; // , 16384, 16416, 16448];
+// let bitmapStart = 16384+31;
+let bitmapStart = 16384 + 7648 + 31;
+for (let i = 0; i < 16; i++) {
+    memoryCellIds.push(bitmapStart + 32*i);
+}
+memoryCellIds.push(24575);
+let tableBody = document.querySelector("#memory-body");
 for (let i = 0; i < memoryCellIds.length; i++) {
+    /*         <tr>
+    <th>SP</th>
+    <td id="mem-0">0</td>
+  </tr
+  */
+    let tableRow = document.createElement("tr");
+    let rowHead = document.createElement("th");
+    rowHead.innerHTML = memoryCellIds[i].toString();
+    tableRow.appendChild(rowHead);
+    let rowCell = document.createElement("td");
+    rowCell.innerHTML = "0";
+    tableRow.appendChild(rowCell);
+    tableBody.appendChild(tableRow);
     let cellId = memoryCellIds[i];
-    memoryCells[cellId] = document.querySelector(`#mem-${cellId}`);
+    // memoryCells[cellId] = document.querySelector(`#mem-${cellId}`);
+    memoryCells[cellId] = rowCell;
 }
 // for (let i = 0; i < 3; i++) {
 //     memoryCells[i] = document.querySelector(`#mem-${i}`);
@@ -36,7 +57,7 @@ for (let i = 0; i < memoryCellIds.length; i++) {
 
 document.onkeypress = function (e) {
     e = e || window.event;
-    console.log(e.keyCode);
+    // console.log(e.keyCode);
     vm.set_key(e.keyCode);
     // use e.keyCode
 };
@@ -64,11 +85,11 @@ runEl.addEventListener("click", event => {
             requestAnimationFrame(executeSteps);
         // }
         frames++;
-        3000
+//        30000i
         for (let i = 0; i < 8000; i++) {
             vm.tick();
         }
-        console.log("**** value from VM: " + vm.peek(256));
+        // console.log("**** value from VM: " + vm.peek(256));
         // for (let i = 0; i < 3; i++) {
         //     memoryCells[i].innerHTML = `${vm.peek(i)}`;
         // }
@@ -76,7 +97,7 @@ runEl.addEventListener("click", event => {
         // for (let i = 256; i < 262; i++) {
         //     memoryCells[i].innerHTML = `${vm.peek(i)}${stackPointer === i ? " < SP" : ""}`;
         // }
-        for (let i = 0; i < memoryCellIds.lengthi - 1; i++) {
+        for (let i = 0; i < memoryCellIds.length - 1; i++) {
             let cellId = memoryCellIds[i];
             memoryCells[cellId].innerHTML = `${vm.peek(cellId)}${stackPointer === cellId ? " < SP" : ""}`;
         }
