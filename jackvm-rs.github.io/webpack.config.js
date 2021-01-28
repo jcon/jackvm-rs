fc7cccd9a3f7e13c676ed93d474f085f96e70282
Jim Connell <james.d.connell@gmail.com>
2021-01-28 14:08:28 -0500

    remove unnecessary plugins.

--- jackvm-rs.github.io/webpack.config.js
+++ jackvm-rs.github.io/webpack.config.js
@@ -1,8 +1,5 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const path = require('path');
 
-const crateDir = path.resolve(__dirname, '../web');
 const srcDir = path.resolve(__dirname, '_js');
 const distDir = path.resolve(__dirname, 'js');
 
@@ -17,16 +14,5 @@ module.exports = {
     asyncWebAssembly: true,
   },
   devtool: "source-map",
-  plugins: [
-    // new CopyWebpackPlugin({
-    //   patterns: [
-    //     {from: 'static', to: distDir },
-    //   ]
-    // }),
-    // new WasmPackPlugin({
-    //   crateDirectory: crateDir,
-    //   outName: 'web',
-    // //   forceMode: 'production',
-    // })
-  ],
+  plugins: [],
 };
