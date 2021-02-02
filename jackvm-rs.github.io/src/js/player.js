c4e6d89f56b8b217677eb8594dcc09c41ec32c1c
Jim Connell <james.d.connell@gmail.com>
2021-02-02 12:29:00 -0500

    split out memory debugger.

--- jackvm-rs.github.io/src/js/player.js
+++ jackvm-rs.github.io/src/js/player.js
@@ -1,5 +1,6 @@
 // import * as wasm from "jackvm-player";
 import { JackVirtualMachine, greet } from "jackvm-player";
+import { MemoryDebugger } from './memory-debugger';
 
 const HEIGHT = 256;
 const WIDTH = 512;
@@ -19,53 +20,6 @@ function createCanvas(height, width) {
     return mainCanvas;
 }
 
-class MemoryDebugger {
-    constructor(vm) {
-        this.vm = vm;
-        let registerCellIds = [0, 1, 2, 3, 4]; // 5, 6, 7, 8];
-        let memoryCellIds = [256, 257, 258, 259, 260, 261, 262, 24576]; // , 16384, 16416, 16448];
-
-        this.memoryCells = {};
-        this.initializedMemory = false;
-        this.allMemoryCellIds = [];
-        let tableBody = document.querySelector("#memory-body");
-        if (tableBody !== undefined) {
-            for (let i = 0; i < memoryCellIds.length; i++) {
-                let tableRow = document.createElement("tr");
-                let rowHead = document.createElement("th");
-                rowHead.innerHTML = memoryCellIds[i].toString();
-                tableRow.appendChild(rowHead);
-                let rowCell = document.createElement("td");
-                rowCell.innerHTML = "0";
-                tableRow.appendChild(rowCell);
-                tableBody.appendChild(tableRow);
-                let cellId = memoryCellIds[i];
-
-                this.memoryCells[cellId] = rowCell;
-            }
-            let that = this;
-            registerCellIds.forEach(cellId => {
-                that.memoryCells[cellId] = document.querySelector(`#mem-${cellId}`);
-            });
-
-            this.allMemoryCellIds = registerCellIds.concat(memoryCellIds);
-            this.initializedMemory = true;
-        }
-    }
-
-    update() {
-        if (!this.initializedMemory) {
-            return;
-        }
-        let stackPointer = this.vm.peek(0);
-        for (let i = 0; i < this.allMemoryCellIds.length - 1; i++) {
-            let cellId = this.allMemoryCellIds[i];
-            this.memoryCells[cellId].innerHTML = `${this.vm.peek(cellId)}${stackPointer === cellId ? " < SP" : ""}`;
-        }
-        this.memoryCells[24576].innerHTML = `${this.vm.peek(24575)} (key: ${String.fromCharCode(this.vm.peek(24575))})`;
-    }
-
-}
 
 class Player {
     constructor(parentEl, config = { debugMemory: true }) {
