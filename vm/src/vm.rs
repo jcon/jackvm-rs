378e287a6e5597bc850a904b9f96fb5e8d317e18
Jim Connell <james.d.connell@gmail.com>
2021-02-08 10:12:11 -0500

    clear all memory on restart.

--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -112,7 +112,10 @@ impl VirtualMachine {
     }
 
     pub fn restart(&mut self) {
+        for elem in self.memory.iter_mut() { *elem = 0; }
+
         self.memory[SP] = STACK_START as i16;
+
         match self.addresses.get("Sys.init") {
             Some(addr) => {
                 self.pc = *addr as usize;
