605fb6bfbcec650dc347d75370802cd2e58f91ff
Jim Connell <james.d.connell@gmail.com>
2021-02-16 21:44:52 -0500

    add build script to trigger wasm-opt manually.

---
+++ compiler/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+LIB=compiler_bg
+
+# Builds the NPM package
+wasm-pack build --release
+
+# Run wasm-opt on binary for platforms where wasm-pack didn't do it for us automatically
+# (currently M1 macs).
+# We can safely run it again on a .wasm file that's already been optimized though.
+which wasm-opt > /dev/null
+if [ $? -eq 0 ]; then
+    wasm-opt -Os -o pkg/${LIB}.new.wasm pkg/${LIB}.wasm
+    mv pkg/${LIB}.new.wasm pkg/${LIB}.wasm
+fi
+
+# This is a hack. wasm-pack hard-codes `sideEffects` false into its generated
+# package.json. Webpack 4+'s treeshaking breaks though, because it erronously
+# treeshakes out files we need in this module.
+cat ./pkg/package.json | jq '. + {"sideEffects": true}' > ./pkg/package.new.json
+mv ./pkg/package.new.json ./pkg/package.json
